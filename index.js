f15544d04c36a8037eb6e34d74acdd55d7039727
SarthakPandey1008 <realsarthakpandey1@gmail.com>
2024-10-04 16:57:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const http = require("http");
+const https = require("https"); // Change from http to https
+const fs = require("fs"); // File system module to read SSL files
 const { Server } = require("socket.io");
 const mainRouter = require("./routes/main.router.js");
 
 const yargs = require('yargs');
-const {hideBin} = require("yargs/helpers");
+const { hideBin } = require("yargs/helpers");
 
 const { initRepo } = require("./controllers/init.js");
 const { addRepo } = require("./controllers/add.js");
@@ -22,15 +23,15 @@ dotenv.config();
 yargs(hideBin(process.argv))
 .command("start", "Start a new server", {}, startServer)
 .command("init", "Initialise a new repository", {}, initRepo)
-.command("add <file>", "Add a file to the repository", (yargs)=>{
+.command("add <file>", "Add a file to the repository", (yargs) => {
     yargs.positional("file", {
         describe: "File to add to the staging area",
         type: "string",
     });
 },
-(argv) =>{
-    addRepo(argv.file);
-} )
+    (argv) => {
+        addRepo(argv.file);
+    })
 .command(
     "commit <message>",
     "Commit the staged files",
@@ -40,9 +41,9 @@ yargs(hideBin(process.argv))
             type: "string",
         });
     },
-   (argv) => {
-    commitRepo(argv.message);
-   }
+    (argv) => {
+        commitRepo(argv.message);
+    }
 )
 .command("push", "Push commits to S3", {}, pushRepo)
 .command("pull", "Pull commits from S3", {}, pullRepo)
@@ -59,54 +60,59 @@ yargs(hideBin(process.argv))
         revertRepo(argv.commitID);
     }
 )
-
 .demandCommand(1, "You need at least one command")
 .help().argv;
 
 // startServer function 
-function startServer(){
-const app = express();
-const port = process.env.PORT || 3000;
+function startServer() {
+    const app = express();
+    const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(express.json());
+    app.use(bodyParser.json());
+    app.use(express.json());
 
-const mongoURI = process.env.MONGODB_URI;
+    const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI).then(()=>console.log("MongoDB connected!")).catch((err)=>console.error("Unable to connect : ", err));
+    mongoose.connect(mongoURI).then(() => console.log("MongoDB connected!")).catch((err) => console.error("Unable to connect: ", err));
 
-app.use(cors({origin: "*"}));
+    app.use(cors({ origin: "*" }));
 
+    app.use("/", mainRouter);
 
-app.use("/", mainRouter);
+    let user = "test";
 
-let user = "test";
-const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    }
-});
+    // Load SSL certificate and key
+    const options = {
+        key: fs.readFileSync('ssl.key'), // path to your ssl.key
+        cert: fs.readFileSync('ssl.crt') // path to your ssl.crt
+    };
 
- io.on("connection", (socket)=>{
-    socket.on("joinRoom", (userID)=>{
-        user = userID;
-        console.log("====");
-        console.log(user);
-        console.log("=====");
-        socket.join(userID);
+    // Create HTTPS server
+    const httpsServer = https.createServer(options, app);
+    const io = new Server(httpsServer, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+        }
     });
- });
 
- const db = mongoose.connection;
+    io.on("connection", (socket) => {
+        socket.on("joinRoom", (userID) => {
+            user = userID;
+            console.log("====");
+            console.log(user);
+            console.log("=====");
+            socket.join(userID);
+        });
+    });
 
- db.once("open", async()=>{
-    console.log("CRUD operations called");
- });
+    const db = mongoose.connection;
 
- httpServer.listen(port, ()=>{
-    console.log(`Server is running on PORT ${port}`);
- })
+    db.once("open", async () => {
+        console.log("CRUD operations called");
+    });
 
+    httpsServer.listen(port, () => {
+        console.log(`Server is running on PORT ${port}`);
+    });
 }
