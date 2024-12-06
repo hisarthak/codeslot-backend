1aeb21fa46a7087eeced5ad9db9c1f010b7742b5
Sarthak Pandey <realsarthakpandey1@gmail.com>
2024-12-06 13:31:22 +0530

    changes in repoModel.js

--- models/repoModel.js
+++ models/repoModel.js
@@ -1,35 +1,41 @@
 const mongoose = require("mongoose");
-const { required } = require("yargs");
 const { Schema } = mongoose;
 
-const RepositorySchema = new Schema({
- name: {
-    type: String,
-    required: true,
-    unique: true,
- },
- description: {
-    type: String,
- },
- content:[{
-    type: String,
- },
-],
- visibility: {
-    type: Boolean,
- },
-owner:{
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-},
-issues:[
-    {
-        type:Schema.Types.ObjectId,
-        ref:"Issue"
-    }
-]
-});
+const RepositorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    content: [
+      {
+        type: String,
+      },
+    ],
+    visibility: {
+      type: Boolean,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    issues: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+      },
+    ],
+  },
+  { collation: { locale: "en", strength: 2 } } // Collation to make the name field case-insensitive
+);
+
+// Create a unique index on the name field with collation to enforce case-insensitive uniqueness
+RepositorySchema.index({ name: 1 }, { unique: true, collation: { locale: "en", strength: 2 } });
 
 const Repository = mongoose.model("Repository", RepositorySchema);
 
