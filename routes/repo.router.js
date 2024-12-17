5c12600bd228826239edfaf5e6816bd589844c63
Sarthak Pandey <realsarthakpandey1@gmail.com>
2024-12-17 13:10:05 +0530

    hi

--- routes/repo.router.js
+++ routes/repo.router.js
@@ -5,13 +5,13 @@ const repoRouter = express.Router();
 
 repoRouter.post("/repo/create", repoController.createRepository);
 repoRouter.get("/repo/all", repoController.getAllRepositories);
-repoRouter.get("/repo/user/:reponame", repoController.repoFolderStructure);
-repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
+repoRouter.get("/repo/:id", repoController.fetchRepositoryById); // Place this first
 repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
 repoRouter.get("/repo/user/:userID", repoController.fetchRepositoriesForCurrentUser);
+repoRouter.get("/repo/user/:reponame", repoController.repoFolderStructure); // More generic last
 repoRouter.put("/repo/update/:id", repoController.updateRepositoryById);
 repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
-repoRouter.patch("/repo/toggle/:id", repoController.toggleVisibilityById)
+repoRouter.patch("/repo/toggle/:id", repoController.toggleVisibilityById);
 
 
 module.exports = repoRouter;
